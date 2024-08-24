0caae00e490a6f64136df6e164b32bb736dae4cd
Gabrielle da Silva Alves <gabriellealves2015@gmail.com>
2024-08-24 19:13:56 -0300

    Removed aggregation in dim_order_reasons.

--- models/marts/dim_order_reasons.sql
+++ models/marts/dim_order_reasons.sql
@@ -2,9 +2,9 @@ with
     sales_reason as (
         select
             id_sales_order
-            , aggregated_id_sales_reason
-            , aggregated_reason_name
-            , aggregated_reason_type
+            , id_sales_reason
+            , reason_name
+            , reason_type
         from {{ ref('int_sales_order_reason') }}
     )
 
@@ -12,9 +12,9 @@ with
         select
             {{ dbt_utils.generate_surrogate_key(['id_sales_order']) }} as sk_order_reason
             , id_sales_order
-            , aggregated_id_sales_reason
-            , aggregated_reason_name
-            , aggregated_reason_type
+            , id_sales_reason
+            , reason_name
+            , reason_type
         from sales_reason
     )
 
