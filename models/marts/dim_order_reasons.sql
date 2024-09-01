0e71d0bcac72e5b44a27b270aecadcdda94ba44d
Gabrielle Alves <gabriellealves2015@gmail.com>
2024-09-01 19:34:21 -0300

    Added not informed information in dim_order_reasons and sales order SK.

--- models/marts/dim_order_reasons.sql
+++ models/marts/dim_order_reasons.sql
@@ -8,14 +8,34 @@ with
         from {{ ref('int_sales_order_reason') }}
     )
 
+    , orders as (
+        select distinct
+            sk_sales_order
+            , id_sales_order
+        from {{ ref('fact_order_details') }}
+    )
+
+    , join_tables as (
+        select
+            sales_reason.id_sales_order
+            , orders.sk_sales_order
+            , sales_reason.id_sales_reason
+            , sales_reason.reason_name
+            , sales_reason.reason_type
+        from sales_reason
+        left join orders
+            on sales_reason.id_sales_order = orders.id_sales_order
+    )
+
     , transformation as (
         select
             {{ dbt_utils.generate_surrogate_key(['id_sales_order', 'id_sales_reason']) }} as sk_order_reason
+            , sk_sales_order as fk_sales_order 
             , id_sales_order
             , id_sales_reason
             , reason_name
             , reason_type
-        from sales_reason
+        from join_tables
     )
 
     select *
